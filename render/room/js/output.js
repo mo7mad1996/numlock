e6299f0c43d363945e19b0fb76c14a442e382c32
Mohamed Ibrahim <mo7mad36@gmail.com>
2023-08-28 11:25:26 +0300

    added audio to cotrollers

--- render/room/js/output.js
+++ render/room/js/output.js
@@ -1,5 +1,17 @@
+const audio = document.createElement("audio");
+
+audio.src("/media/click.mp3");
+
 socket.on("add text", (i) => {
-  const span = document.createElement("span");
-  span.innerText = i;
-  document.body.appendChild(span);
+  audio.play();
+  switch (i) {
+    case "backspace":
+      document.body.removeChild(document.body.lastElementChild);
+      break;
+    default:
+      const span = document.createElement("span");
+
+      span.innerText = i;
+      document.body.appendChild(span);
+  }
 });
