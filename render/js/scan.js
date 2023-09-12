83ba8d479023bcfb15cacdf82be308fbcc3a5f6a
Mohamed Ibrahim <mo7mad36@gmail.com>
2023-09-12 11:50:26 +0300

    added sound to scaner

--- render/js/scan.js
+++ render/js/scan.js
@@ -1,5 +1,8 @@
+const sound = new Audio("/media/scaner.mp3");
+
 const scaner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
 scaner.render((url) => {
+  sound.play();
   socket.emit("go to output", url, (_) => {
     window.location.replace(`/room/controller/${url}`);
   });
