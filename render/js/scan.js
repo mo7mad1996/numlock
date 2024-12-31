b1c52923f35f136b9d197feb50d837e354157826
Mohamed Ibrahem <65599462+mo7mad1996@users.noreply.github.com>
2024-12-31 15:17:03 +0200

    scanner update

--- render/js/scan.js
+++ render/js/scan.js
@@ -2,8 +2,10 @@ const sound = new Audio("/media/scaner.mp3");
 
 const scaner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 250 });
 scaner.render((url) => {
+const room = url.split("/")[url.split("/").length - 1];
+
   sound.play();
-  socket.emit("go to output", url, (_) => {
+  socket.emit("go to output", room, (_) => {
     window.location.href(`${url}`);
   });
   scaner.clear();
